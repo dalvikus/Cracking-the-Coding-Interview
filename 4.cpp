d4a21ad646f0535f36c526648caac87581fe62a4
Gwangho Kim <dalvikus@gmail.com>
2017-01-31 15:51:32 -0800

    4.2

--- 4.cpp
+++ 4.cpp
@@ -31,6 +31,112 @@ int main(void)
 
     4.3 Given a sorted (increasing order) array, write an algorithm to create a binary tree with minimal height.
 
+    1 2 3 4 5 6 7
+
+        4
+        |
+    +-------+
+    |       |
+    2       6
+    |       |
+  +---+   +---+
+  |   |   |   |
+  1   3   5   7
+
+    1 2 3 4 5 6
+
+        4
+        |
+    +-------+
+    |       |
+    2       6
+    |       |
+  +---+   +---+
+  |   |   |   |
+  1   3   5   -
+
+
+    1 2 3 4 5 6 7 8 9 A B C D E F
+
+                   8
+                   |
+             +-----------+
+             |           |
+             4           C
+             |           |
+          +-----+     +-----+
+          |     |     |     |
+          2     6     A     E
+          |     |     |     |
+        +---+ +---+ +---+ +---+
+        |   | |   | |   | |   |
+        1   3 5   7 9   B D   F
+
+    1 2 3 4 5 6 7 8 9 A B C D E
+
+                   8
+                   |
+             +-----------+
+             |           |
+             4           C
+             |           |
+          +-----+     +-----+
+          |     |     |     |
+          2     6     A     E
+          |     |     |     |
+        +---+ +---+ +---+ +---+
+        |   | |   | |   | |   |
+        1   3 5   7 9   B D   -
+
+    1 2 3 4 5 6 7 8 9 A B C D
+
+                   8
+                   |
+             +-----------+
+             |           |
+             4           C
+             |           |
+          +-----+     +-----+
+          |     |     |     |
+          2     6     A     E
+          |     |     |     |
+        +---+ +---+ +---+ +---+
+        |   | |   | |   | |   |
+        1   3 5   7 9   B -   -
+
+int a[] = {1, 2, 3};
+void add_node(Node **node_ptr, int min, int max)
+{
+cout << "min = " << min << "; max = " << max << endl;
+    if (min > max)
+        throw "min > max";
+
+    int n = max - min + 1;
+    int i = min + (int) ceil(n / 2);
+    Node *p = new Node(a[i]);
+    p->left = 0;
+    p->right = 0;
+    assert(*node_ptr == 0);
+    *node_ptr = p;
+    if (n == 1) {
+        cout << "neight left nor right" << endl;
+        return;
+    }
+    if (i - 1 >= min) {
+        cout << "left:" << endl;
+        add_node(&p->left, min, i - 1);
+    } else {
+        cout << "no left" << endl;
+    }
+    if (max >= i + 1) {
+        cout << "right:" << endl;
+        add_node(&p->right, i + 1, max);
+    } else {
+        cout << "no right" << endl;
+    }
+}
+
+
     4.4 Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth (i.e., if you have a tree with depth D, you'll have D linked list).
 
     4.5 Write an algorithm to find the 'next' node (i.e., in-order successor) of a given node in a binary search tree where each node has a link to its parent.
@@ -44,6 +150,8 @@ int main(void)
 
 #include <iostream>
 using namespace std;
+#include <assert.h> // assert
+#include <math.h>   // ceil
 
 struct Node
 {
@@ -78,6 +186,38 @@ int balanced_depth(Node* node)
     return left_depth == right_depth ? 1 + left_depth : -1;
 }
 
+int a[] = {1, 2, 3};
+void add_node(Node **node_ptr, int min, int max)
+{
+cout << "min = " << min << "; max = " << max << endl;
+    if (min > max)
+        throw "min > max";
+
+    int n = max - min + 1;
+    int i = min + (int) ceil(n / 2);
+    Node *p = new Node(a[i]);
+    p->left = 0;
+    p->right = 0;
+    assert(*node_ptr == 0);
+    *node_ptr = p;
+    if (n == 1) {
+        cout << "neight left nor right" << endl;
+        return;
+    }
+    if (i - 1 >= min) {
+        cout << "left:" << endl;
+        add_node(&p->left, min, i - 1);
+    } else {
+        cout << "no left" << endl;
+    }
+    if (max >= i + 1) {
+        cout << "right:" << endl;
+        add_node(&p->right, i + 1, max);
+    } else {
+        cout << "no right" << endl;
+    }
+}
+
 int main(void)
 {
     Node    root;
@@ -86,4 +226,11 @@ int main(void)
         cout << "unbalanced" << endl;
     else
         cout << "balanced with depth = " << depth << endl;
+
+    {
+        Node *node = 0;
+        add_node(&node, 0, sizeof(a) / sizeof(int) - 1);
+        if (node)
+            delete node;
+    }
 }
