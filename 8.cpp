ac24bae2e1d9b348de4af9e401710026d373e78c
Gwangho Kim <dalvikus@gmail.com>
2017-02-01 12:32:13 -0800

    8.7 solved

--- 8.cpp
+++ 8.cpp
@@ -10,6 +10,8 @@
     output: ()()(), ()(()), (())(), ((()))
 
     typo: (()()) must be added!
+
+8.6 Given an infinite number of quaters(25 cents), dimes(10 cents), nickets(5 cents) and pennies(1 cent), write code to calculate the number of ways of representing n cents.
  */
 
 #include <set>
@@ -92,7 +94,7 @@ void parenthesis(unsigned open, unsigned close, string s, vector<string>& ss)
 int ex8_5(int argc, const char *argv[])
 {
     if (argc == 1) {
-        cerr << "Ex. 8.3: " << argv[0] << " n" << endl;
+        cerr << "Ex. 8.5: " << argv[0] << " n" << endl;
         return 1;
     }
     char *endptr;
@@ -111,10 +113,86 @@ int ex8_5(int argc, const char *argv[])
 }
 #endif  // 8.5
 
+#if 1   // 8.7
+struct Coin
+{
+    unsigned m_quarters;
+    unsigned m_dimes;
+    unsigned m_nickels;
+    unsigned m_pennies;
+    Coin(unsigned quarters, unsigned dimes, unsigned nickels, unsigned pennies) : m_quarters(quarters), m_dimes(dimes), m_nickels(nickels), m_pennies(pennies) {}
+    void print(void)
+    {
+        cout << "(" << m_quarters << ", " << m_dimes << ", " << m_nickels << ", " << m_pennies << "): " << (25 * m_quarters + 10 * m_dimes + 5 * m_nickels + m_pennies) << endl;
+    }
+};
+
+void distribute_pennies(unsigned cents, unsigned quarters, unsigned dimes, unsigned nickels, vector<Coin>& ways)
+{
+    ways.push_back(Coin(quarters, dimes, nickels, cents));
+}
+void distribute_nickels(unsigned cents, unsigned quarters, unsigned dimes, unsigned nickels, vector<Coin>& ways)
+{
+    unsigned c = cents;
+    unsigned n = nickels;
+    while (c >= 5) {
+        ++n;
+        distribute_pennies(c - 5, quarters, dimes, n, ways);
+        c -= 5;
+    }
+    if (c > 0)
+        distribute_pennies(c, quarters, dimes, n, ways);
+}
+void distribute_dimes(unsigned cents, unsigned quarters, unsigned dimes, unsigned nickels, vector<Coin>& ways)
+{
+    unsigned c = cents;
+    unsigned d = dimes;
+    while (c >= 10) {
+        ++d;
+        distribute_nickels(c - 10, quarters, d, nickels, ways);
+        c -= 10;
+    }
+}
+void distribute_quarters(unsigned cents, unsigned quarters, unsigned dimes, unsigned nickels, vector<Coin>& ways)
+{
+    unsigned c = cents;
+    unsigned q = quarters;
+    while (c >= 25) {
+        ++q;
+        distribute_dimes(c - 25, q, dimes, nickels, ways);
+        c -= 25;
+    }
+}
+int ex8_7(int argc, const char *argv[])
+{
+    if (argc < 3) {
+        cerr << "Ex. 8.7: " << argv[0] << " not_used cents" << endl;
+        return 1;
+    }
+    char *endptr;
+    long cents = strtol(argv[2], &endptr, 10);
+    if (*endptr)
+        cerr << "WARN: |" << argv[2] << "|: all not used" << endl;
+    if (cents < 0) {
+        cerr << "ERR: cents must be non-negative; " << cents << endl;
+        return 1;
+    }
+    cout << "cents = " << cents << endl;
+    vector<Coin>    ways;
+    distribute_quarters(cents, 0, 0, 0, ways);
+    cout << "# of ways = " << ways.size() << endl;
+    for (vector<Coin>::iterator it = ways.begin(); it != ways.end(); ++it)
+        it->print();
+
+    return 0;
+}
+#endif  // 8.7
+
 int main(int argc, const char *argv[])
 {
     ex8_3(argc, argv);
     ex8_5(argc, argv);
+    ex8_7(argc, argv);
 
     return 0;
 }
